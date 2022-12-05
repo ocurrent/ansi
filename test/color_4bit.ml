0adefd516e907ca10ef50e099a16bbd66e248ed5
Antonin Décimo <antonin@tarides.com>
2022-12-05 17:39:59 +0100

    Add html expect tests

---
+++ test/color_4bit.ml
@@ -0,0 +1,54 @@
+let set_foreground_color ch ?(bright=false) c =
+  Printf.fprintf ch "\x1b[%s%dm" (if bright then "1;" else "") c
+
+let set_background_color ch ?(bright=false) c =
+  Printf.fprintf ch "\x1b[%s%dm" (if bright then "1;" else "") c
+
+let reset_output ch =
+  output_string ch "\x1b[0m"
+
+let output_string ch str =
+  output_string ch str;
+  reset_output ch
+
+let output ch =
+  for i = 30 to 37 do
+    set_foreground_color ch i;
+    output_string ch "\u{2588}"
+  done;
+  for i = 90 to 97 do
+    set_foreground_color ch i;
+    output_string ch "\u{2588}"
+  done;
+  output_char ch '\n';
+  for i = 40 to 47 do
+    set_background_color ch i;
+    output_string ch " "
+  done;
+  for i = 100 to 107 do
+    set_background_color ch i;
+    output_string ch " "
+  done;
+  output_char ch '\n';
+
+  for i = 30 to 37 do
+    set_foreground_color ch ~bright:true i;
+    output_string ch "\u{2588}"
+  done;
+  for i = 90 to 97 do
+    set_foreground_color ch ~bright:true i;
+    output_string ch "\u{2588}"
+  done;
+  output_char ch '\n';
+  for i = 40 to 47 do
+    set_background_color ch ~bright:true i;
+    output_string ch " "
+  done;
+  for i = 100 to 107 do
+    set_background_color ch ~bright:true i;
+    output_string ch " "
+  done;
+  output_char ch '\n';
+  ()
+
+let () = output stdout
