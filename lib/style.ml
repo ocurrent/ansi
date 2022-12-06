23a7f5ad2bc4f8617a213509565cacdf54fe4566
Antonin Décimo <antonin@tarides.com>
2022-12-06 01:30:09 +0100

    Use pretty-printers for CSS rules

--- lib/style.ml
+++ lib/style.ml
@@ -1,37 +1,67 @@
-let css = {|
-  pre span.fg-black   { color: rgb(0, 0, 0) }
-  pre span.fg-red     { color: rgb(205, 0, 0) }
-  pre span.fg-green   { color: rgb(0, 205, 0) }
-  pre span.fg-yellow  { color: rgb(205, 205, 0) }
-  pre span.fg-blue    { color: rgb(0, 0, 238) }
-  pre span.fg-magenta { color: rgb(205, 0, 205) }
-  pre span.fg-cyan    { color: rgb(0, 205, 205) }
-  pre span.fg-white   { color: rgb(229, 229, 229) }
+type rgb = int * int * int
 
-  pre span.fg-bright-black   { color: rgb(127, 127, 127) }
-  pre span.fg-bright-red     { color: rgb(255, 0, 0) }
-  pre span.fg-bright-green   { color: rgb(0, 255, 0) }
-  pre span.fg-bright-yellow  { color: rgb(255, 255, 0) }
-  pre span.fg-bright-blue    { color: rgb(92, 92, 255) }
-  pre span.fg-bright-magenta { color: rgb(255, 0, 255) }
-  pre span.fg-bright-cyan    { color: rgb(0, 255, 255) }
-  pre span.fg-bright-white   { color: rgb(255, 255, 255) }
+type t =
+  {
+    black : rgb;
+    red : rgb;
+    green : rgb;
+    yellow : rgb;
+    blue : rgb;
+    magenta : rgb;
+    cyan : rgb;
+    white : rgb;
+  }
 
-  pre span.bg-black   { background: rgb(0, 0, 0) }
-  pre span.bg-red     { background: rgb(205, 0, 0) }
-  pre span.bg-green   { background: rgb(0, 205, 0) }
-  pre span.bg-yellow  { background: rgb(205, 205, 0) }
-  pre span.bg-blue    { background: rgb(0, 0, 238) }
-  pre span.bg-magenta { background: rgb(205, 0, 205) }
-  pre span.bg-cyan    { background: rgb(0, 205, 205) }
-  pre span.bg-white   { background: rgb(229, 229, 229) }
+let pp_rgb fmt (r, g, b) =
+  Fmt.pf fmt "rgb(%d, %d, %d)" r g b
 
-  pre span.bg-bright-black   { background: rgb(127, 127, 127) }
-  pre span.bg-bright-red     { background: rgb(255, 0, 0) }
-  pre span.bg-bright-green   { background: rgb(0, 255, 0) }
-  pre span.bg-bright-yellow  { background: rgb(255, 255, 0) }
-  pre span.bg-bright-blue    { background: rgb(92, 92, 255) }
-  pre span.bg-bright-magenta { background: rgb(255, 0, 255) }
-  pre span.bg-bright-cyan    { background: rgb(0, 255, 255) }
-  pre span.bg-bright-white   { background: rgb(255, 255, 255) }
-|}
+let pp_escape fmt dof bright name rgb =
+  Fmt.pf fmt "pre span.%s-%s%s { %s: %a }\n"
+    (match dof with `Fg -> "fg" | `Bg -> "bg")
+    (if bright then "bright-" else "")
+    name
+    (match dof with `Fg -> "color" | `Bg -> "background")
+    pp_rgb
+    rgb
+
+let pp_colors fmt dof bright t =
+  pp_escape fmt dof bright "black" t.black;
+  pp_escape fmt dof bright "red" t.red;
+  pp_escape fmt dof bright "green" t.green;
+  pp_escape fmt dof bright "yellow" t.yellow;
+  pp_escape fmt dof bright "blue" t.blue;
+  pp_escape fmt dof bright "magenta" t.magenta;
+  pp_escape fmt dof bright "cyan" t.cyan;
+  pp_escape fmt dof bright "white" t.white
+
+let pp fmt (colors, bright_colors) =
+  pp_colors fmt `Fg false colors;
+  pp_colors fmt `Fg true bright_colors;
+  pp_colors fmt `Bg false colors;
+  pp_colors fmt `Bg true bright_colors
+
+let default =
+  {
+    black = 0, 0, 0;
+    red = 205, 0, 0;
+    green = 0, 205, 0;
+    yellow = 205, 205, 0;
+    blue = 0, 0, 238;
+    magenta = 205, 0, 205;
+    cyan = 0, 205, 205;
+    white = 229, 229, 229;
+  }
+
+let default_bright =
+  {
+    black = 127, 127, 127;
+    red = 255, 0, 0;
+    green = 0, 255, 0;
+    yellow = 255, 255, 0;
+    blue = 92, 92, 255;
+    magenta = 255, 0, 255;
+    cyan = 0, 255, 255;
+    white = 255, 255, 255;
+  }
+
+let css = Fmt.str "%a" pp (default, default_bright)
