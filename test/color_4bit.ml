a46a9515e101d7203bed59583053512dfda7f068
Antonin Décimo <antonin@tarides.com>
2022-12-14 15:30:09 +0100

    Further fix for bright/bold confusion

--- test/color_4bit.ml
+++ test/color_4bit.ml
@@ -1,8 +1,8 @@
-let set_foreground_color ch ?(bright=false) c =
-  Printf.fprintf ch "\x1b[%s%dm" (if bright then "1;" else "") c
+let set_foreground_color ch c =
+  Printf.fprintf ch "\x1b[%dm" c
 
-let set_background_color ch ?(bright=false) c =
-  Printf.fprintf ch "\x1b[%s%dm" (if bright then "1;" else "") c
+let set_background_color ch c =
+  Printf.fprintf ch "\x1b[%dm" c
 
 let reset_output ch =
   output_string ch "\x1b[0m"
@@ -27,25 +27,6 @@ let output ch =
   done;
   for i = 100 to 107 do
     set_background_color ch i;
-    output_string ch " "
-  done;
-  output_char ch '\n';
-
-  for i = 30 to 37 do
-    set_foreground_color ch ~bright:true i;
-    output_string ch "\u{2588}"
-  done;
-  for i = 90 to 97 do
-    set_foreground_color ch ~bright:true i;
-    output_string ch "\u{2588}"
-  done;
-  output_char ch '\n';
-  for i = 40 to 47 do
-    set_background_color ch ~bright:true i;
-    output_string ch " "
-  done;
-  for i = 100 to 107 do
-    set_background_color ch ~bright:true i;
     output_string ch " "
   done;
   output_char ch '\n';
