f74877416901a62996f2c7f86743ba82703e66f6
Antonin Décimo <antonin@tarides.com>
2023-03-01 11:50:42 +0100

    Support faint and double underline styles

--- test/color_4bit.ml
+++ test/color_4bit.ml
@@ -1,8 +1,12 @@
-let set_foreground_color ch c =
-  Printf.fprintf ch "\x1b[%dm" c
+type style = [ `Normal | `Faint ]
 
-let set_background_color ch c =
-  Printf.fprintf ch "\x1b[%dm" c
+let style_to_code : style -> int = function `Normal -> 00 | `Faint -> 02
+
+let set_foreground_color ch ~styles c =
+  List.iter (fun style -> Printf.fprintf ch "\x1b[%d;%dm" (style_to_code style) c) styles
+
+let set_background_color ch ~styles c =
+  List.iter (fun style -> Printf.fprintf ch "\x1b[%d;%dm" (style_to_code style) c) styles
 
 let reset_output ch =
   output_string ch "\x1b[0m"
@@ -11,25 +15,28 @@ let output_string ch str =
   output_string ch str;
   reset_output ch
 
-let output ch =
+let output ?(styles=[`Normal]) ?(fg="\u{2588}") ?(bg=" ") ch =
   for i = 30 to 37 do
-    set_foreground_color ch i;
-    output_string ch "\u{2588}"
+    set_foreground_color ch ~styles i;
+    output_string ch fg
   done;
   for i = 90 to 97 do
-    set_foreground_color ch i;
-    output_string ch "\u{2588}"
+    set_foreground_color ch ~styles i;
+    output_string ch fg
   done;
   output_char ch '\n';
   for i = 40 to 47 do
-    set_background_color ch i;
-    output_string ch " "
+    set_background_color ch ~styles i;
+    output_string ch bg
   done;
   for i = 100 to 107 do
-    set_background_color ch i;
-    output_string ch " "
+    set_background_color ch ~styles i;
+    output_string ch bg
   done;
   output_char ch '\n';
   ()
 
-let () = output stdout
+let () =
+  output ~styles:[`Normal] stdout;
+  output ~styles:[`Faint] stdout;
+  ()
